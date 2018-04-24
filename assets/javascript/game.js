041d5b329b3c9e30c26984d66ad1629745a47cb5
Jesse <laethaka@gmail.com>
2018-04-24 10:01:30 -0700

    trying to make a hit replace all letters but currently broken

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 //VARIABLES SETUP
 var wordBank = ['MEPHISTO', 'AESHMA', 'BEELZEBUB', 'BELPHEGOR', 'LEVIATHAN', 'MOLOCH', 'PANZUZU', 'VASSAFOR', 'SATHANAS', 'DIABLO'];
 var secretWord = wordBank[Math.floor(Math.random()*10)];
-var displayWord = ""
+var displayWord = "";
+var hitIndices = [];
 
 while (displayWord.length < secretWord.length) {
     displayWord += '-'
 }
 
-//HANGMAN LINE DISPLAY
+//HANGMAN LINE SETUP
 document.getElementById("hangmanLine").innerHTML = displayWord;
 
 //KEYBOARD LISTENING
@@ -19,9 +20,22 @@ document.onkeypress = function(event) {
     var keyPressed = String.fromCharCode(event.keyCode).toUpperCase();
     if (secretWord.includes(keyPressed)) {
         //KEY PRESSED IS A HIT
-        alert('a hit!')
-        var hitIndex = secretWord.indexOf(keyPressed);
-        displayWord.replaceAt(hitIndex, keyPressed);
+                                    // hitIndex = secretWord.indexOf(keyPressed);
+        for(let i=0; i<secretWord.length;i++) {
+            if (secretWord[i] === keyPressed) hitIndices.push(i);
+        }
+                                    //displayWord = displayWord.replaceAt(hitIndex, keyPressed);
+        for(let j=0; j<displayWord.length;j++) {
+            if ()
+        }
+        document.getElementById("hangmanLine").innerHTML = displayWord;
+                                    //ONLY FIRST HIT GETS REVEALED
+    // } else if (keyPressed == "") {
+    //     //KEY PRESSED ISN'T A LETTER
+    //     break;
+    // } else {
+    //     //KEY PRESSED IS A MISS
+    //     document.getElementById('pentagramStatus').src='assets/images/pentagram1.jpg'
     };
 };
 
