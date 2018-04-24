f5aa10502d48e6cf9cfb31e3263c57372df1eb08
Jesse <laethaka@gmail.com>
2018-04-24 00:07:56 -0700

    I think displayWord is being changed, but it isn't updating on the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //VARIABLES SETUP
-var wordBank = ['Mephisto', 'Aeshma', 'Beelzebub', 'Belphegor', 'Leviathan', 'Moloch', 'Pazuzu', 'Vassafor', 'Sathanas', 'Diablo'];
+var wordBank = ['MEPHISTO', 'AESHMA', 'BEELZEBUB', 'BELPHEGOR', 'LEVIATHAN', 'MOLOCH', 'PANZUZU', 'VASSAFOR', 'SATHANAS', 'DIABLO'];
 var secretWord = wordBank[Math.floor(Math.random()*10)];
 var displayWord = ""
 
@@ -7,12 +7,21 @@ while (displayWord.length < secretWord.length) {
     displayWord += '-'
 }
 
+//HANGMAN LINE DISPLAY
+document.getElementById("hangmanLine").innerHTML = displayWord;
+
 //KEYBOARD LISTENING
-document.onkeydown = function(event) {
-    var keyPressed = String.fromCharCode(event.keyCode);
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
 }
 
+document.onkeypress = function(event) {
+    var keyPressed = String.fromCharCode(event.keyCode).toUpperCase();
+    if (secretWord.includes(keyPressed)) {
+        //KEY PRESSED IS A HIT
+        alert('a hit!')
+        var hitIndex = secretWord.indexOf(keyPressed);
+        displayWord.replaceAt(hitIndex, keyPressed);
+    };
+};
 
-
-console.log(secretWord);
-console.log(displayWord);
